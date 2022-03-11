7dc38fcebd67c4d4815594d847b5b6c4cb45da13
hansicheng <hansisicheng@msgsz.com>
2022-03-11 17:29:11 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+    Outlet, Link
+} from "react-router-dom";
+import NotFind from "./pages/notFind.tsx"
+import Main from "./pages/main.tsx";
+import Logo from "./pages/logo.tsx";
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Main from "./pages/main.tsx";
+import Logo from "./pages/logo.tsx";
+import NotFind from "./pages/notFind.tsx";
+import Show from "./pages/show.tsx"
+import PageA from "./pages/pageA.tsx"
+import PageB from "./pages/pageB.tsx"
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <Routes>
+          <Route path="/" element={ <App />} >
+              <Route path="/" element={<Main />} >
+                  <Route index element={<Show />} />
+                  <Route path="/PageA:Id" element={<PageA />} >
+
+                  </Route>
+                  <Route path="/PageB:Id" element={<PageB />} />
+              </Route>
+              <Route path="/logo" element={<Logo />} />
+              <Route path="*" element={<NotFind />} />
+          </Route>
+      </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
